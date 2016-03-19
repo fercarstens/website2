7c69467eedcb276ee62e0aa446c3747cee97ae55
fercarstens <fernandocarstensps@gmail.com>
2016-03-19 17:50:55 -0430

    cool

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def index2(request):
+	posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).order_by('-fecha_publicado')
+	return render(request, 'blog/index2.html',  {'posts': posts})
